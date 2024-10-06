4501e352cb22da626521eb3a1a75672e2d080c22
Davy-71993 <59330475+Davy-71993@users.noreply.github.com>
2024-10-06 21:06:20 +0300

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,18 +1,8 @@
-import React from 'react';
-import Container from '../Container';
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { APP_NAME } from '@/lib/defaults';
-import { APP_NAME } from '@/lib/defaults';
-import Image from 'next/image';
-import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer id='footer' className='w-full bg-primary min-h-96'>
-      
-    </footer>
+    <div></div>
   );
 };
 
